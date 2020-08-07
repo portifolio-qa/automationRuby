5d493c262ba189f59872fa26c63adb5cbdec03a3
rosedias <rosedias.info@gmail.com>
2020-08-07 16:29:42 -0300

    "validacao campos Name & email"

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -9,6 +9,36 @@ describe "Form" do
     expect(find("#msg").visible?).to be true
 
     expect find("#msg").text).to have_content "Campos preenchidos com sucesso, login realizado. BEM VINDO!!"
+
+    page.save_screenshot("log/senha_incorreta.png")
     
   end
+
+  it 'campo nome obrigatorio', :temp do
+    visit '/login'
+
+    fill_in 'userId', whith: ''
+    fill_in 'password', whith: 'error'
+
+    click_button "Login"
+
+    expect find("#msg").text).to have_content 'O campo *nome é obrigatorio, por favor informe um login!'
+
+    page.save_screenshot("log/campo_obrigatorio.png")
+
 end
+
+  it 'campo email obrigatorio' do
+    visit '/login'
+
+    fill_in 'userId', whith: 'Teste'
+    fill_in 'password', whith: ''
+
+    click_button "Login"
+
+    expect find("#msg").text).to have_content 'O campo *email é obrigatorio, por favor informe um email '
+
+    page.save_screenshot("log/email_obrigatorio.png")
+
+  end
+
