c34c2305f59e8dfd89b2da41d6acf493f9b0fabb
rosedias <rosedias.info@gmail.com>
2020-08-07 16:10:15 -0300

    form

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -1,6 +1,14 @@
-describe "Formulario" do 
-  it "acessar a pagina" do
-    visit "http://localhost:8080/usuarios"
-    sleep 5
+describe "Form" do
+  it "login com sucesso" do
+    visti "" 
+    fill_in "username", whith: "Teste"
+    fill_in "email", whith: "teste@gmail.com"
+
+    click_button "Login"
+
+    expect(find("#msg").visible?).to be true
+
+    expect find("#msg").text).to have_content "Campos preenchidos com sucesso, login realizado. BEM VINDO!!"
+    
   end
 end
---
+++ spec/page_spec.rb
@@ -0,0 +1,6 @@
+describe "Page" do 
+  it "acessar a pagina" do
+    visit "http://localhost:8080/usuarios"
+    sleep 5
+  end
+end
